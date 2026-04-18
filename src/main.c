8c4ec4ceacadff63357f9e18842a9b6f8b7e556f
HenriqueIsonaka <henrique.isonaka@gmail.com>
2026-04-17 23:48:31 -0300

    Commit 4.1: roda em velocidades diferentes

--- src/main.c
+++ src/main.c
@@ -1,41 +1,53 @@
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
 
-#define SLEEP_TIME_MS 1000
+#include <zephyr/kernel.h>        // Funções básicas do Zephyr (ex: k_msleep, k_thread, etc.)
+#include <zephyr/device.h>        // API para obter e utilizar dispositivos do sistema
+#include <zephyr/drivers/gpio.h>  // API para controle de pinos de entrada/saída (GPIO)
+#include <pwm_z42.h>        // Biblioteca personalizada com funções de controle do TPM (Timer/PWM Module)
 
-// Define o LED usando Device Tree
-#define LED0_NODE DT_ALIAS(led0)
+#define PTA_NODE 
+// Define o valor do registrador MOD do TPM para configurar o período do PWM
+#define TPM_MODULE 1000  // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
+// Valores de duty cycle correspondentes a diferentes larguras de pulso
+uint16_t vel_25  = TPM_MODULE/4;  
+uint16_t vel_10  = TPM_MODULE/10; 
+uint16_t vel_100  = TPM_MODULE/1;
 
-// Verifica se o LED está definido no Device Tree
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-#else
-#error "Unsupported board: led0 devicetree alias is not defined"
-#endif
-
-void main(void)
+int main(void)
 {
-    int ret;
+    // Inicializa o módulo TPM2 com:
+    // - base do TPMx
+    // - fonte de clock PLL/FLL (TPM_CLK)
+    // - valor do registrador MOD
+    // - tipo de clock (TPM_CLK)
+    // - prescaler de 1 a 128 (PS)
+    // - modo de operação EDGE_PWM
+    pwm_tpm_Init(TPM1, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
-    // Verifica se o device está pronto
-    if (!gpio_is_ready_dt(&led)) {
-        printk("Error: LED device %s is not ready\n", led.port->name);
-        return;
+    pwm_tpm_Ch_Init(TPM1, 0, TPM_PWM_H, GPIOB, 0);
+    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 3);// até aqui está ok(pino PTB0 E PTB3)
+    
+    const struct device *gpioa_apontador = DEVICE_DT_GET(DT_NODELABEL(gpioa));
+    
+    if (!device_is_ready(gpioa_apontador)) {
+        return 0;
     }
 
-    // Configura o pino como saída
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        printk("Error %d: failed to configure LED pin\n", ret);
-        return;
-    }
+    gpio_pin_configure(gpioa_apontador, 1, GPIO_OUTPUT_ACTIVE);// motor a in1(pino PTA1)
+    gpio_pin_configure(gpioa_apontador, 2, GPIO_OUTPUT_INACTIVE);//motor a in2(pino PTA2)
+    gpio_pin_configure(gpioa_apontador, 12, GPIO_OUTPUT_ACTIVE);//motor b in3(pino PTA12)
+    gpio_pin_configure(gpioa_apontador, 4, GPIO_OUTPUT_INACTIVE);// motor b in4(pino PTA 4)
 
-    printk("LED blinking on %s pin %d\n", led.port->name, led.pin);
 
-    while (1) {
-        // Toggle do LED usando a nova API
-        gpio_pin_toggle_dt(&led);
-        k_msleep(SLEEP_TIME_MS);
+    // Loop infinito
+    for(;;) {
+        pwm_tpm_CnV(TPM1, 0, vel_100);
+        pwm_tpm_CnV(TPM2, 1, vel_100);
+        k_msleep(4000);
+        pwm_tpm_CnV(TPM1, 0, vel_25);
+        k_msleep(4000);
+        pwm_tpm_CnV(TPM1, 0, vel_10);
+        k_msleep(4000);
     }
+    return 0;
 }
