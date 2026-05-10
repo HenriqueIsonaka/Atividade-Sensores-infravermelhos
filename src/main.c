3a7d83d9b31c9131f13a9b6e8042f892fb767650
HenriqueIsonaka <henrique.isonaka@gmail.com>
2026-05-10 14:58:03 -0300

    Commit atividade 4(2): Teste da resposta dos motores aos sensores infravermelhos

--- src/main.c
+++ src/main.c
@@ -4,31 +4,41 @@
 #include <pwm_z42.h>
 
 #define TPM_MODULE          1000
+#define VELOCIDADE_MOTOR    600     // 60% de potência para o carrinho 
 #define PORTA_A             DT_NODELABEL(gpioa)
 
 // Definição dos LEDs via DeviceTree
-static const struct gpio_dt_spec led_blue = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
-static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 
 #define SENSOR_A_PIN        12      //PTA12
 #define SENSOR_B_PIN        13      //PTA13
 
-typedef enum{ Desligado, Azul, Verde, Ambos } carrinho_t;     // Máquina de Estados
+typedef enum{ Parado, A_Ativo, B_Ativo, Ambos } carrinho_t;     // Máquina de Estados
 
 int main(void){
+    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);      //1. PWM (Motores)
+    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 2);      // ENA  (Motor A)
+    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 3);      // ENB  (Motor B)
 
-    const struct device *gpioa_dev = DEVICE_DT_GET(PORTA_A);                    
+    const struct device *gpioa_dev = DEVICE_DT_GET(PORTA_A);                    //2. Direção dos Motores (Porta A)
     if(!device_is_ready(gpioa_dev)) return 0;
     
+    //Motor A --> Para Frente
+    gpio_pin_configure(gpioa_dev, 1, GPIO_OUTPUT_ACTIVE);       // IN1 = 1
+    gpio_pin_configure(gpioa_dev, 2, GPIO_OUTPUT_INACTIVE);     // IN2 = 0
+    //Motor B --> Para Frente
+    gpio_pin_configure(gpioa_dev, 4, GPIO_OUTPUT_ACTIVE);       // IN3 = 1
+    gpio_pin_configure(gpioa_dev, 5, GPIO_OUTPUT_INACTIVE);     // IN4 = 0
 
     gpio_pin_configure(gpioa_dev, SENSOR_A_PIN, GPIO_INPUT | GPIO_PULL_UP);     //3. Sensores (Entradas com Pull-up)
     gpio_pin_configure(gpioa_dev, SENSOR_B_PIN, GPIO_INPUT | GPIO_PULL_UP);
 
-    if(!gpio_is_ready_dt(&led_blue) || !gpio_is_ready_dt(&led_green)) return 0;  //4. LEDs
-    gpio_pin_configure_dt(&led_blue, GPIO_OUTPUT_INACTIVE);                 
+    if(!gpio_is_ready_dt(&led_red) || !gpio_is_ready_dt(&led_green)) return 0;  //4. LEDs
+    gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);                 
     gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE);
 
-    carrinho_t estado_atual = Desligado;           // VARIÁVEIS DE CONTROLE
+    carrinho_t estado_atual = Parado;           // VARIÁVEIS DE CONTROLE
     bool Sensor_A, Sensor_B;                    // VARIÁVEIS DE CONTROLE
 
     while(1){       
@@ -36,26 +46,34 @@ int main(void){
         Sensor_B = (gpio_pin_get(gpioa_dev, SENSOR_B_PIN) == 0);    // o Sensor retorna 0 quando detecta algo, isso inverte essa lógica, agora retornando 1.
 
         if(Sensor_A && Sensor_B)            estado_atual = Ambos;   //2. DEFINIÇÃO DO PRÓXIMO ESTADO
-        else if(Sensor_A && !Sensor_B)      estado_atual = Azul;
-        else if(!Sensor_A && Sensor_B)      estado_atual = Verde;
-        else                                estado_atual = Desligado; 
+        else if(Sensor_A && !Sensor_B)      estado_atual = A_Ativo;
+        else if(!Sensor_A && Sensor_B)      estado_atual = B_Ativo;
+        else                                estado_atual = Parado; 
 
         switch(estado_atual){                                       //3. AÇÕES DA MÁQUINA DE ESTADOS
-            case Azul:
-                gpio_pin_set_dt(&led_blue, 1);      // LED azul
+            case A_Ativo:
+                pwm_tpm_CnV(TPM2, 0, VELOCIDADE_MOTOR);  
+                pwm_tpm_CnV(TPM2, 1, 0);
+                gpio_pin_set_dt(&led_red, 1);      // LED Vermelho
                 gpio_pin_set_dt(&led_green, 0);
                 break;
-            case Verde:
-                gpio_pin_set_dt(&led_blue, 0);
+            case B_Ativo:
+                pwm_tpm_CnV(TPM2, 0, 0);  
+                pwm_tpm_CnV(TPM2, 1, VELOCIDADE_MOTOR);
+                gpio_pin_set_dt(&led_red, 0);
                 gpio_pin_set_dt(&led_green, 1);         // LED Verde 
                 break;
             case Ambos:
-                gpio_pin_set_dt(&led_blue, 1);      // LED Azul claro
-                gpio_pin_set_dt(&led_green, 1);        
+                pwm_tpm_CnV(TPM2, 0, VELOCIDADE_MOTOR);  
+                pwm_tpm_CnV(TPM2, 1, VELOCIDADE_MOTOR);
+                gpio_pin_set_dt(&led_red, 1);      // LED Amarelo
+                gpio_pin_set_dt(&led_green, 1);         // LED Amarelo
                 break;
-            case Desligado:
+            case Parado:
             default:
-                gpio_pin_set_dt(&led_blue, 0);      // LED Apagado;
+                pwm_tpm_CnV(TPM2, 0, 0);  
+                pwm_tpm_CnV(TPM2, 1, 0);
+                gpio_pin_set_dt(&led_red, 0);      // LED Apagado;
                 gpio_pin_set_dt(&led_green, 0);
                 break;
         }
